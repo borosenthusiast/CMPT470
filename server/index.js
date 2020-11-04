b0f0181fc529a786dd33377f54c0e5d780d576d4
yla382 <yoonhong_lee@sfu.ca>
2020-11-04 15:21:20 -0800

    TODO address page looking different when running on node js

--- server/index.js
+++ server/index.js
@@ -1,27 +1,33 @@
 var express = require('express');
 var app = express();
-var http = require('http');
+var indexRoute = require('./routes/indexRoute');
 
-app.get('/request', function(req, response) {  //server asks for test data
-	var options = {
-		host: 'localhost',
-		port: 8080,
-		path: '/receive'
-	}; 
+app.use(express.json());
 
-	http.get(options, res => {
-		let data = "";
+app.use('/', indexRoute);
+
+
+// var http = require('http');
+
+// app.get('/request', function(req, response) {  //server asks for test data
+// 	var options = {
+// 		host: 'localhost',
+// 		port: 8080,
+// 		path: '/receive'
+// 	}; 
+
+// 	http.get(options, res => {
+// 		let data = "";
 		
-		res.on("data", d => {
-			data += d;
-		});
+// 		res.on("data", d => {
+// 			data += d;
+// 		});
 
-		res.on("end", () => {
-			response.send(data);
-			//console.log(data);
-		})
-	});
-});
-
+// 		res.on("end", () => {
+// 			response.send(data);
+// 			//console.log(data);
+// 		})
+// 	});
+// });
 
 app.listen(3000);  // app.listen(port, [host], [backlog], [callback]])
---
+++ server/routes/indexRoute.js
@@ -0,0 +1,26 @@
+var router = require('express').Router();
+var path = require('path');
+var file_path = "/../views/";
+
+router.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + file_path + "homepage/index.html"));
+});
+
+router.get('/homestyle.css', function(req, res) {
+	res.sendFile(path.join(__dirname + file_path + "homepage/homestyle.css"));
+});
+
+router.get('/homescript.js', function(req, res) {
+	res.sendFile(path.join(__dirname + file_path + "homepage/homescript.js"));
+});
+
+router.get('/images/home1.jpg', function(req, res) {
+	res.sendFile(path.join(__dirname + file_path + "images/home1.jpg"));
+});
+
+router.get('/images/home2.jpg', function(req, res) {
+	res.sendFile(path.join(__dirname + file_path + "images/home2.jpg"));
+});
+
+
+module.exports = router;
