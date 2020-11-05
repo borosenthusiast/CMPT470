1d2a2d4750d61f880bf77682853e8bfb37bcbda1
ohsu <orion_hsu@sfu.ca>
2020-11-04 22:39:37 -0800

    Update server/index.js

--- server/index.js
+++ server/index.js
@@ -16,29 +16,62 @@ app.use('/', indexRoute);
 
 app.use('/register', registerRoute);
 
+
+class AuthHandler {
+    login (req, res) {
+        let username = req.body.username;
+        let password = req.body.password;
+        // TODO: Get user infromation from database
+        let testName = 'hi';
+        let testPw = 'bye'
+        //--------------------------------------
+        
+        if (username && password) {
+            if (username === testName && password === testPw) {
+                console.log("Test login success!")
+                let token = jwt.sign({username: username},
+                    config.secret, {
+                        expiresIn: '1h'
+                    }
+                );
+                res.json({
+                    success: true,
+                    message: 'Authentication Success',
+                    token: token
+                });
+            }
+            else {
+                res.sendStatus(403).json({
+                    success: false,
+                    message: 'Incorrect Login Username or Password.'
+                });
+            }
+        }
+        else {
+            res.sendStatus(400).json({
+                success: false,
+                message: 'Authentication Failed - Incorrect Request'
+            })
+        }
+    }
+
+    index(req, res) {
+        res.json({
+            success:true,
+            message: 'Index page'
+        });
+    }
+
+}
+
+
+let handler = new AuthHandler();
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+app.use(bodyParser.json());
+
 app.post('/login', handler.login);
 app.get('/chktoken', middleware.checkToken, handler.index);
-// var http = require('http');
-
-// app.get('/request', function(req, response) {  //server asks for test data
-// 	var options = {
-// 		host: 'localhost',
-// 		port: 8080,
-// 		path: '/receive'
-// 	}; 
-
-// 	http.get(options, res => {
-// 		let data = "";
-		
-// 		res.on("data", d => {
-// 			data += d;
-// 		});
-
-// 		res.on("end", () => {
-// 			response.send(data);
-// 			//console.log(data);
-// 		})
-// 	});
-// });
 
 app.listen(3000);  // app.listen(port, [host], [backlog], [callback]])
