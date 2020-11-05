72578b4944c7f8c0f671cc760da823ff22bff906
ohsu <ohsu@sfu.ca>
2020-11-04 17:35:08 -0800

    add client javascript for post request - authentication & updated package.json for added requirements

--- server/views/homepage/homescript.js
+++ server/views/homepage/homescript.js
@@ -6,4 +6,12 @@ function tologin(){
 	document.getElementById('home').classList.add('loginhome');
 	document.getElementById('loginform').style.visibility ="visible";
 	document.getElementById('loginbox').style.visibility ="visible";
+
+	function submitLogin() {
+		username = document.getElementById('username').value;
+		password = document.getElementById('password').value;
+		
+	}
+
+	document.getElementById('loginbutton').addEventListener('click', submitLogin);
 }
