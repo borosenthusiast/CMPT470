73ad3e685a1e1226f31624a73fa1d30854b7782d
ohsu <ohsu@sfu.ca>
2020-11-08 11:18:32 -0800

    fixed ajax query to have proper formatting

--- server/views/homepage/homescript.js
+++ server/views/homepage/homescript.js
@@ -9,21 +9,36 @@ function tologin(){
 //var formData = JSON.stringify($("#myForm").serializeArray());
 var $ = jQuery;
 
+function getFormUser() {
+	return document.getElementById('username').value;
+	
+}
+
+function getFormPw() {
+	return document.getElementById('password').value;
+}
+
 $(document).ready(function() {
 	console.log("document ready")
 
 	$('#loginform').submit(function(e){
 		e.preventDefault();
-		alert("ajax jquery request sent")
-		var form = $(this);
-		var url = form.attr('action');
-		console.log(form.username)
+		alert("ajax jquery request sent");
+		var user = getFormUser();
+		var pass = getFormPw();
+		console.log(user);
+		console.log(pass);
+		var json_query = {};
+		json_query.password = pass;
+		json_query.username = user;
+		JSON.stringify(json_query);
+		console.log(json_query);
 		
 		$.ajax({
 			url: "http://localhost:3000/login",
 			type: 'POST',
 			dataType: 'json',
-			data: form.serialize(),
+			data: json_query,
 			error : function(err) {
 			  console.log('Error!', err)
 			},
