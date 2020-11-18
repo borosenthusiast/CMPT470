9bc329dada7eefa600054eb92f4d79e5ef739d98
yla382 <yoonhong_lee@sfu.ca>
2020-11-18 14:04:01 -0800

    images from upload files removed

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -1,11 +1,7 @@
 exports.submitProfile = async (req, res) => {
-	let token = req.headers['x-access-token'] || req.headers['authorization'];
-	
-	if (token.startsWith('Bearer ')) {
-        token = token.slice(8, token.length);
-    }
-	
+
+
 	console.log(req.body.bio);
 	console.log(req.file);
-	console.log(token);
+	console.log(req.decoded.id);
 }
