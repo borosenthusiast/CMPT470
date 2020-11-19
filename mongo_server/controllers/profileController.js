a60988ca1db712c0ff6dceef3dc2010cbd31d4b0
yla382 <yoonhong_lee@sfu.ca>
2020-11-19 10:23:55 -0800

    add profile to mongodb implemented

--- mongo_server/controllers/profileController.js
+++ mongo_server/controllers/profileController.js
@@ -1,4 +1,11 @@
+var Profile = require('../models/profile.js');
+
 exports.addProfile = async(req, res) => {
+	let profile = req.body;
+	let result = await Profile.createProfile(profile);
+
+	console.log(result);
+
 	res.status(200).json({
 			success: true,
 			message: 'Profile created'
