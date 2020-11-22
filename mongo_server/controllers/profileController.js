4c80898722758a43dc0d1bf5d4a4500dfcd2537a
yla382 <yoonhong_lee@sfu.ca>
2020-11-21 17:52:04 -0800

    add profile updated

--- mongo_server/controllers/profileController.js
+++ mongo_server/controllers/profileController.js
@@ -2,12 +2,29 @@ var Profile = require('../models/profile.js');
 
 exports.addProfile = async(req, res) => {
 	let profile = req.body;
-	let result = await Profile.createProfile(profile);
-
-	console.log(result);
-
-	res.status(200).json({
-			success: true,
-			message: 'Profile created'
-	});
+	
+	let query_result = await Profile.checkProfileExists(profile.userId);
+	if(query_result === null) {
+		console.log('Profile does not exists can be proceed to add');
+		let result = await Profile.createProfile(profile);
+		console.log(result.insertedId);
+		if(result.insertedId) {
+			console.log('Profile created in mongoDB');
+			res.status(200).json({
+				success: true,
+				message: 'Profile created in mongoDB'
+			});
+		} else {
+			res.status(500).json({
+				success: false,
+				message: 'Profile was not able to added in mongoDB'
+			});
+		}
+	} else {
+		console.log('Profile already exists in mongoDB');
+		res.status(500).json({
+			success: false,
+			message: 'Profile already exists in mongoDB'
+		});
+	}
 }
--- mongo_server/models/profile.js
+++ mongo_server/models/profile.js
@@ -7,14 +7,28 @@ var Profile = function(profile) {
 	this.pet = profile.pet;
 }
 
+
 Profile.createProfile = async (profile) => {
 	try {
-		let profile_collection = await mongo_db.mongo_collection('ProfilePage');
+		let profile_collection = await mongo_db.mongo_collection('ProfilePage')
 		let result = await profile_collection.insertOne(profile);
 		return result;
 	} catch(err) {
 		console.log(err);
 		console.log("Error in Profile.createProfile");
+	}
+}
+
+Profile.checkProfileExists = async (userId) => {
+	try {
+		let profile_collection = await mongo_db.mongo_collection('ProfilePage')
+		let query = {userId: userId};
+		let projection = {_id: 1, userId: 1};
+		let result = await profile_collection.findOne(query, projection);
+		return result;
+	} catch (err) {
+		console.log(err);
+		console.log("Error in Profile.checkProfileExists");
 	}
 }
 
