dffe4b754e1a5b512816f35efa3e77095029ce57
ohsu <ohsu@sfu.ca>
2020-12-01 00:40:35 -0800

    added logout js function to non login pages (if needed)

--- server/views/adoptionpage/adopt.js
+++ server/views/adoptionpage/adopt.js
@@ -26,7 +26,19 @@ function weightpullup(){
 }
 
 
-
+function logout() {
+  $.ajax({
+      url: "/logout",
+      type: "GET",
+      error: function(err) {
+          console.log("Failed to log out with error: ", err)
+      },
+      success: function(data) {
+          localStorage.removeItem('token');
+          sessionStorage.removeItem('token');
+      }
+  });
+}
 
 function deck(filterstat){
 	/*
--- server/views/viewprofilepage/viewprofile.js
+++ server/views/viewprofilepage/viewprofile.js
@@ -1,3 +1,16 @@
 //var edit = document.getElementById("edit");
 //edit.addEventListener("click", displayDetails);
 
+function logout() {
+  $.ajax({
+      url: "/logout",
+      type: "GET",
+      error: function(err) {
+          console.log("Failed to log out with error: ", err)
+      },
+      success: function(data) {
+          localStorage.removeItem('token');
+          sessionStorage.removeItem('token');
+      }
+  });
+}
