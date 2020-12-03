fd38b266b888bd8ca4b9f9bb56b6923a12a89006
yla382 <yoonhong_lee@sfu.ca>
2020-12-03 13:27:04 -0800

    dynamically updating rows on front-end side implemented

--- server/routes/adminRoute.js
+++ server/routes/adminRoute.js
@@ -15,6 +15,6 @@ router.get('/userlist.js', function(req,res) {
 	res.sendFile(path.join(__dirname + file_path + "userlistpage/userlist.js"));
 });
 
-router.get('/allusers', userController.getAllUsers);
+router.get('/getallusers', userController.getAllUsers);
 
 module.exports = router;
