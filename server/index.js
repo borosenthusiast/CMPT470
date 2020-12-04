c5827066768b15d919f8b3fe4fe86d31b681adaf
ohsu <ohsu@sfu.ca>
2020-12-04 13:02:59 -0800

    fix registration bug

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use('/', indexRoute);
 
 app.use('/register', registerRoute);
 
-app.use('/profile', middleware.checkToken, profileRoute); // use middleware checktoken for protected pages that require authentication from the user.
+app.use('/profile', profileRoute); // use middleware checktoken for protected pages that require authentication from the user.
 
 app.use('/petprofile', middleware.checkToken, petProfileRoute);
 
