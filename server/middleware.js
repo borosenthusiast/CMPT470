3035db618487662e3143d18f4c55624beb562550
yla382 <yoonhong_lee@sfu.ca>
2020-12-04 19:00:41 -0800

    fuck this

--- server/middleware.js
+++ server/middleware.js
@@ -3,38 +3,44 @@ const config = require('./config.js');
 var User = require('./models/user.js')
 
 const account_type = {
-	ADMIN: 'Admin',
-	ADOPT: 'Adopt',
-	USER: 'User'
+    ADMIN: 'Admin',
+    ADOPT: 'Adopt',
+    USER: 'User'
 };
 
-let checkToken = (req, res, next) => {
-    let headers = req.headers['x-access-token'] || req.headers['authorization'];
-    // if (token.startsWith('Bearer ')) {
-    //     token = token.slice(8, token.length);
-    // }
-    token = JSON.parse(headers).token;
+const getUser = async (id) => {
+    user = await (User.getUserbyId(id));
+    return user;
+}
+
+const checkToken = (req, res, next) => {
+    console.log('tokencheck');
+    if (typeof req.cookies !== "undefined") {
+        var token = req.cookies['Authentication'];
+    }
     //console.log(token);
-    if (token) {
-        jwt.verify(token, config.secret, (err, decoded) => {
+    if (token && typeof token !== "undefined") {
+        jwt.verify(token, config.secret, async (err, decoded) => {
         if (err) {
             console.log("err in token");
-            return res.json({
-            success: false,
-            message: 'Token is not valid'
+            return res.status(403).json({
+                success: false,
+                message: 'Token is not valid'
             });
         } else {
             req.decoded = decoded;
             //console.log("decoded: " + JSON.stringify(decoded.id));
-            let user = User.getUserbyId(decoded.id);
+            let user = await getUser(decoded.id);
+            //console.log(user);
+            req.uid = decoded.id;
             req.account_type = user.account_type;
-            console.log("User matched from DB: " + JSON.stringify(user));
+            //console.log("User matched from DB: " + JSON.stringify(user));
             next();
         }
         });
     } else {
         console.log("No authentication token was provided.");
-        return res.json({
+        return res.status(403).json({
         success: false,
         message: 'Auth token is not supplied'
         });
