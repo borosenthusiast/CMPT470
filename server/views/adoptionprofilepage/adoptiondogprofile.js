b1ce180d1872407328b491095b6fca4e60151bf1
twl8 <timothy_wai-kin_lam@sfu.ca>
2020-12-06 22:08:11 -0800

    Added nav-bar to adoptiondogprofile page

--- server/views/adoptionprofilepage/adoptiondogprofile.js
+++ server/views/adoptionprofilepage/adoptiondogprofile.js
@@ -13,7 +13,21 @@ function logout() {
 	});
   }
 
+function userprofile(){
+	$.ajax({
 
+        url: "/profile/getUID",
+        type: "GET",
+        dataType: 'json',
+        error : function(err) {
+            console.log('Error!', err)
+        },
+        success: function(data) {
+        	window.location.href = "/admin/view/" + data.uid; 
+        }
+    });	
+
+}
 
 var imageIndex;
 var imageArray = new Array();
